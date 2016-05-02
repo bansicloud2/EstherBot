a636bf7c27d9d8dd6c34f0d3a6dd2584367308e1
bansicloud2 <chet@zendodo.com>
2016-05-02 14:05:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`Let me get my supervisor on the line, okay?.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
